52cc547fee34f369d4f46e64069e69abf4b4fec1
Your Name <b>
2023-05-29 23:53:08 +0300

    add edit birthday

--- acme_project/birthday/views.py
+++ acme_project/birthday/views.py
@@ -1,14 +1,21 @@
-from django.shortcuts import render
-
+from django.shortcuts import get_object_or_404, render
+from birthday.models import Birthday
 from birthday.forms import BirthdayForm
 from birthday.utils import calculate_birthday_countdown
 
 
-def birthday(request):
-    form = BirthdayForm(request.POST or None)
+def birthday(request, pk=None):
+
+    if pk is not None:
+        instance = get_object_or_404(Birthday, pk=pk)
+    else:
+        instance = None
+    
+    form = BirthdayForm(request.POST or None, instance=instance)
     context = {'form': form}
 
     if form.is_valid():
+        form.save()
         birthday_countdown = calculate_birthday_countdown(
             # ...и передаём в неё дату из словаря cleaned_data.
             form.cleaned_data['birthday']
@@ -16,3 +23,13 @@ def birthday(request):
         context.update({'birthday_countdown': birthday_countdown})
 
     return render(request, 'birthday/birthday.html', context=context)
+
+
+def birthday_list(request):
+    birthdays = Birthday.objects.all()
+    context = {'birthdays': birthdays}
+    return render(request, 
+           'birthday/birthday_list.html',
+           context)
+
+#TODO:delete birthday
