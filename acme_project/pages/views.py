1af0cbcb8fecc79ce2488c8c2eca9d27594393c8
Винокуров Николай Андреевич <nvinokurov@sigma-it.ru>
2023-06-03 18:31:43 +0300

    Add auth

--- acme_project/pages/views.py
+++ acme_project/pages/views.py
@@ -1,4 +1,17 @@
+from typing import Any, Dict
 from django.shortcuts import render
+from django.views.generic import TemplateView
+
+from birthday.models import Birthday
+
+
+class HomepageView(TemplateView):
+    template_name = 'pages/index.html'
+
+    def get_context_data(self, **kwargs: Any) -> Dict[str, Any]:
+        context = super().get_context_data(**kwargs)
+        context['total_count'] = Birthday.objects.count()
+        return context
 
 
 def homepage(request):
